9a181d8718c9b542c173e6c58c1aecdff3af6f5b
heyongkang <heyongkang@didachuxing.com>
2018-10-29 21:05:53 +0800

    005

--- 005/reverse.py
+++ 005/reverse.py
@@ -4,5 +4,12 @@
 @author: heyongkang
 @create: 2018/10/29-8:52 PM
 @file: reverse.py
-@function: 
+@function: 输入三个整数x,y,z，请把这三个数由小到大输出。
 '''
+
+l=[]
+for i in range(3):
+    x = input("请输入三个数:")
+    l.append(x)
+l.sort(reverse=False)
+print l
