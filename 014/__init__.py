306084b3781899e3a17830278ad2eab10c13a828
heyongkang <heyongkang@didachuxing.com>
2018-11-05 22:14:08 +0800

    014

---
+++ 014/__init__.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+
+'''
+@author: heyongkang
+@create: 2018/11/3-10:54 AM
+@file: __init__.py.py
+@function: 
+'''
---
+++ 014/factorization.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+
+"""
+@author: heyongkang
+@create: 2018/11/3-10:57 AM
+@file: factorization.py
+@function: 将一个正整数分解质因数。输入90,打印出90=2*3*3*5
+"""
+
+
+
