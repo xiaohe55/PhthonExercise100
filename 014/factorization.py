9237f22c3f8c92703b214fe780338dbe751a1da4
heyongkang <heyongkang@didachuxing.com>
2018-11-05 23:02:07 +0800

    014

--- 014/factorization.py
+++ 014/factorization.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 @author: heyongkang
@@ -8,4 +8,18 @@
 """
 
 
+def divPrime(num):
+    lt = []
+    print(num, '=', end=' ')
+    while num != 1:
+        for i in range(2, int(num + 1)):
+            if num % i == 0:  # i是num的一个质因数
+                lt.append(i)
+                num = num / i  # 将num除以i，剩下的部分继续分解
+                break
+    for i in range(0, len(lt) - 1):
+        print(lt[i], '*', end=' ')
 
+    print(lt[-1])
+
+divPrime(18)
