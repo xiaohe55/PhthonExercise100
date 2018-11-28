636246f0abf5a39a50e788e083cc38cca2ea9797
heyongkang <heyongkang@didachuxing.com>
2018-11-28 10:01:45 +0800

    032

---
+++ 032.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+'''
+@author: heyongkang
+@create: 2018/11/28-9:58 AM
+@file: 032.py
+@function: 按相反的顺序输出列表的值。reverse
+'''
+li = list(input("请输入："))
+print(type(li))
+li.reverse()
+print(li)
